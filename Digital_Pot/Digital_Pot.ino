66f07e6cfe4dc88184c5b8102e6b7a5ea3e8c52b
Mel <mel.matt007@gmail.com>
2018-01-13 15:22:10 -0800

    Add files via upload

---
+++ Digital_Pot/Digital_Pot.ino
@@ -0,0 +1,36 @@
+#include <SPI.h>
+
+byte address = 0x00;
+int CS= 10;
+
+void setup()
+{
+pinMode (CS, OUTPUT);
+SPI.begin();
+}
+
+void loop()
+{
+digitalPotWrite(128);
+/*for (int i = 0;ano i <= 128; i++)
+{
+digitalPotWrite(i);
+delay(10);
+}
+delay(500);
+for (int i = 128; i >= 0; i--)
+{
+digitalPotWrite(i);
+delay(10);
+}*/
+}
+
+int digitalPotWrite(int value)
+{
+digitalWrite(CS, LOW);
+SPI.transfer(address);
+SPI.transfer(value);
+digitalWrite(CS, HIGH);
+}
+
+
